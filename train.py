07eb3a7d6c68b45949591b9676287970d3327dbb
Van Duc <127331546+vvduc1803@users.noreply.github.com>
2023-03-10 06:32:35 +0700

    Update train.py

--- train.py
+++ train.py
@@ -13,8 +13,8 @@ from model import Yolov8_cls
 def main():
 
     # Create tensorboard file to keep track process training
-    # writer = SummaryWriter(f'runs_drop/')
-    # step = 0
+    writer = SummaryWriter(f'runs_drop/')
+    step = 0
 
     # Setup data
     train_loader, val_loader, test_loader, class_labels, test_targets = Dataset_Load(
@@ -36,33 +36,33 @@ def main():
         load_checkpoint(config.CHECKPOINT_FILE, model_yolo, optimizer, config.LR, config.DEVICE)
 
     # Add model graph to tensorboard
-    # writer.add_graph(model_yolo, torch.randn((1, 3, 224, 224)).to(config.DEVICE))
+    writer.add_graph(model_yolo, torch.randn((1, 3, 224, 224)).to(config.DEVICE))
 
 
-    # for epoch in range(config.EPOCHS):
-    #
-    #     # Training process
-    #     train_loss, train_acc = train_step(model_yolo, train_loader, loss_fn, optimizer, config.DEVICE)
-    #
-    #     # Validation process
-    #     val_loss, val_acc = val_step(model_yolo, val_loader, loss_fn, config.DEVICE)
-    #
-    #     # Add information to tensorboard
-    #     writer.add_scalar(f'Training Loss', train_loss, global_step=step)
-    #     writer.add_scalar(f'Validation Loss', val_loss, global_step=step)
-    #     writer.add_scalar(f'Training Accuracy', train_acc, global_step=step)
-    #     writer.add_scalar(f'Validation Accuracy', val_acc, global_step=step)
-    #     step += 1
-    #
-    #     # Print information
-    #     print(f'Epoch: {epoch+1}')
-    #     print(f'Train loss: {train_loss:.4f} | Train accuracy: {train_acc * 100:.2f}%')
-    #     print(f'Validation loss: {val_loss:.4f} | Validation accuracy: {val_acc * 100:.2f}%')
-    #
-    #     # Sve model each 5 epochs
-    #     if (epoch + 1) % 5 == 0 & config.SAVE_MODEL:
-    #         save_checkpoint(model_yolo, optimizer, "model_checkpoint_drop.pt")
-    #     print('--------------------------------------------------------------------')
+    for epoch in range(config.EPOCHS):
+    
+        # Training process
+        train_loss, train_acc = train_step(model_yolo, train_loader, loss_fn, optimizer, config.DEVICE)
+    
+        # Validation process
+        val_loss, val_acc = val_step(model_yolo, val_loader, loss_fn, config.DEVICE)
+    
+        # Add information to tensorboard
+        writer.add_scalar(f'Training Loss', train_loss, global_step=step)
+        writer.add_scalar(f'Validation Loss', val_loss, global_step=step)
+        writer.add_scalar(f'Training Accuracy', train_acc, global_step=step)
+        writer.add_scalar(f'Validation Accuracy', val_acc, global_step=step)
+        step += 1
+    
+        # Print information
+        print(f'Epoch: {epoch+1}')
+        print(f'Train loss: {train_loss:.4f} | Train accuracy: {train_acc * 100:.2f}%')
+        print(f'Validation loss: {val_loss:.4f} | Validation accuracy: {val_acc * 100:.2f}%')
+    
+        # Sve model each 5 epochs
+        if (epoch + 1) % 5 == 0 & config.SAVE_MODEL:
+            save_checkpoint(model_yolo, optimizer, "model_checkpoint_drop.pt")
+        print('--------------------------------------------------------------------')
 
     # Final testing process
     test_loss, test_acc, y_preds_tensor = test_step(model_yolo, test_loader, loss_fn, config.DEVICE)
